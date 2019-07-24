0aee4334d239a5ce411db5a68a1d8b6b23d9dea3
Diego Rojas <diego.gustavo.rojas@gmail.com>
2019-07-24 13:24:16 -0700

    add code review from ryan still have to figure out a few problems with javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
-// var checkboxes = document.getElementsByName('employee');
-// var selected = [];
-//   for (var i=0; i<checkboxes.length; i++) {
-//      if (checkboxes[i].checked) {
-//         selected.push(checkboxes[i].value);
-//      }
-//   }
+// var toppingsConstant = {
+//   pepperoni: 4,
+//   sausage: 2,
+//   bacon: 5,
+//   onion: 1
+// }
 
 //business logic
 
-function Pizza(selectsize) {
-  this.size = selectsize
+function Pizza(selectsize, toppings) {
+  this.size = selectsize;
+  this.toppings = toppings;
+  this.price = 0;
 }
+
+Pizza.prototype.CalcPrice = function() {
+  if (this.size === "small") {
+    this.price = 10;
+  } else if (this.size === "medium") {
+    this.price = 15;
+  } else {
+    this.price = 20;
+  }
+
+  {
+
+  toppingsArray.length * 2 this.price;
+  // Have to figure out how Ryan made this value to be multiplied by two then added to this.price.
+}
+//   this.toppings.forEach(function(top) {
+//     // this.price += toppingsConstant[top];
+//     this.price +=[top];
+  }
 
 //user interface logic
 
@@ -20,32 +40,17 @@ $(document).ready(function() {
 
     event.preventDefault();
 
-    var inputSize = ($("#size").val());
+    var inputSize = $("#size").val();
 
-    var newPizza = new Pizza(inputSize);
-
-    var price = 0
-
+    var toppingsArray = [];
     $("input:checkbox[name=topping]:checked").each(function() {
-      price = parseInt($(this).val())
-      for (var i = 0; i < ".checkbox".length; i++) {
-        if (".checkbox" [i].checked) {
-          price.push(".checkbox" [i].value);
-        }
-      }
-
-
-      if (newPizza.size === "small") {
-        price += 10;
-      } else if (newPizza.size === "medium") {
-        price += 15;
-      } else {
-        price += 20;
-      }
-
-      $("#show-price").text("The price of your pizza is $" + price + ".");
-
-      // console.log(price);
+      toppingsArray.push($(this).val());
     });
+
+    var userPizza = new Pizza(inputSize, toppingsArray);
+    userPizza.CalcPrice();
+
+    $("#show-price").text("The price of your pizza is $" + userPizza.price + ".");
+
   });
 });
